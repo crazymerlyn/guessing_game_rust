7b93ce17fe73a5405b13c1c353d1cacb0538caeb
CrazyMerlyn <ankitgoel616@gmail.com>
2014-11-02 15:04:35 -0500

    Basic game complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 use std::io;
+use std::rand;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please enter your guess:");
 
-    let input = io::stdin().read_line().ok().expect("Failed to read input!");
-    println!("You guessed: {}", input);
+    let secret_number = (rand::random::<uint>() % 100u) + 1u;
+
+    loop {
+        println!("Please enter your guess:");
+
+        let input = io::stdin().read_line().ok().expect("Failed to read input!");
+        let input_num: Option<uint> = from_str(input.as_slice().trim());
+
+        let num = match input_num {
+            Some(num) => num,
+            None      => {
+                println!("Please input a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", input);
+
+        match cmp(num, secret_number) {
+            Less    => println!("Too small!"),
+            Greater => println!("Too big!"),
+            Equal   => {
+                println!("You win");
+                return;
+            }
+        }
+    }
+}
+
+fn cmp(a: uint, b: uint) -> Ordering {
+    if a < b { Less }
+    else if a > b { Greater }
+    else { Equal }
 }
