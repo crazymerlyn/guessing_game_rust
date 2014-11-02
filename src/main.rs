0652f74b94f76ef58ef9bddffe8d5e11ff1ef924
CrazyMerlyn <ankitgoel616@gmail.com>
2014-11-02 14:07:31 -0500

    Used std::io to take input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!")
+    println!("Guess the number!");
+    println!("Please enter your guess:");
+
+    let input = io::stdin().read_line().ok().expect("Failed to read input!");
+    println!("You guessed: {}", input);
 }
